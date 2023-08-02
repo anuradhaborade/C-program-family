5b9b35f6eea72e33cff77476b304e90c7209aa85
Anuradha Borade <anu18121992@gmail.com>
2023-08-03 03:07:48 +0530

    c++ and c difference Function overloading
    1. Function overloading
    2. usage of format specifier in c
    3. operator overloading

--- C++/Addition.cpp
+++ C++/Addition.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
+
 /* Addition of two numbers */
+/* Function overloading*/
 int addition(int val1,int val2)
+{
+    return val1+val2;
+};
+
+float addition(float val1,float val2)
+{
+    return val1+val2;
+};
+
+double addition(double val1,double val2)
 {
     return val1+val2;
 };
@@ -9,9 +21,17 @@ int addition(int val1,int val2)
 int main()
 {
     int A,B;
+    float C,D;
+    double E,F;
     /*<< and >> are Bitwise operator but it is very good example of operator overloading */
     cin>>A>>B;
+    cin>>C>>D;
+    cin>>E>>F;
     cout<< "Addition of "<<A<<" and "<<B<<endl;
     cout<<addition(A,B)<<endl;
+    cout<< "Addition of "<<C<<" and "<<D<<endl;
+    cout<<addition(C,D)<<endl;
+    cout<< "Addition of "<<E<<" and "<<F<<endl;
+    cout<<addition(E,F)<<endl;
     return 0;
 }
--- C/Addition.c
+++ C/Addition.c
@@ -2,19 +2,37 @@
 #include <stdlib.h>
 
 #include <stdio.h>
-/* Addition of two numbers */
-int addition(int val1,int val2)
+/* Addition of two Integer numbers */
+int integer_addition(int val1,int val2)
 {
     return val1+val2;
 };
 
+/* Addition of two Float numbers */
+float float_addition(float val1,float val2)
+{
+    return val1+val2;
+};
+
+/* Addition of two double numbers */
+double double_addition(double val1,double val2)
+{
+    return val1+val2;
+};
 
 int main()
 {
     int A,B;
-
+    float C,D;
+    double E,F;
     scanf("%d%d",&A,&B);
+    scanf("%f%f",&C,&D);
+    scanf("%lf%lf",&E,&F);
     printf("Addition of %d and %d\n",A,B);
-    printf("%d",addition(A,B));
+    printf("%d\n",integer_addition(A,B));
+    printf("Addition of %f and %f\n",C,D);
+    printf("%f\n",float_addition(C,D));
+    printf("Addition of %lf and %lf\n",E,F);
+    printf("%lf",double_addition(E,F));
     return 0;
 }
